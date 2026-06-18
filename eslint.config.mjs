aa136f6a89da7165c70739b8042e2bc54fe37770
Cezar Augusto <boss@cezaraugusto.net>
2026-06-17 23:46:16 -0300

    Apply eslint-config-auditor formatting and lint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
 import ts from 'eslint-config-auditor/ts'
+import stylistic from 'eslint-config-auditor/stylistic'
+import vitest from 'eslint-config-auditor/vitest'
 
 export default [
   {ignores: ['dist/**', 'coverage/**', '**/*.json']},
-  ...ts
+  ...ts,
+  ...stylistic,
+  ...vitest.map((c) => ({
+    ...c,
+    files: ['**/*.test.{js,jsx,ts,tsx}', '**/*.spec.{js,jsx,ts,tsx}']
+  }))
 ]
